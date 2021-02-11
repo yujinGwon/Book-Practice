343a63950d5ccbac91b2ade23b4074c77986e9d4
yujinGwon <67323448+yujinGwon@users.noreply.github.com>
2021-02-11 21:23:18 +0900

    CBV로 블로그 포스트 목록 페이지 만들기

--- 장고+부트스트랩(이지스퍼블리싱)/blog/views.py
+++ 장고+부트스트랩(이지스퍼블리싱)/blog/views.py
@@ -1,18 +1,30 @@
+# from django.shortcuts import render
+# from .models import Post
+#
+# # Create your views here.
+#
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
+#
+#     return render(
+#         request,
+#         'blog/post_list.html',
+#         {
+#             'posts' : posts,
+#         }
+#     )
+#
+
+#############################################
+
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
-# Create your views here.
-
-def index(request):
-    posts = Post.objects.all().order_by('-pk')
-
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts' : posts,
-        }
-    )
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+    # template_name = 'blog/post_list.html'
 
 def single_post_page(request, pk):
     post = Post.objects.get(pk=pk)
@@ -24,3 +36,4 @@ def single_post_page(request, pk):
             'post': post,
         }
     )
+
