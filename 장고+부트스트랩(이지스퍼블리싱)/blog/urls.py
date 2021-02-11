4c083e43a359ce66c392b6dedd8733dca938f4db
yujinGwon <67323448+yujinGwon@users.noreply.github.com>
2021-02-11 20:09:47 +0900

    대문 페이지와 자기소개 페이지 만들기

--- 장고+부트스트랩(이지스퍼블리싱)/blog/urls.py
+++ 장고+부트스트랩(이지스퍼블리싱)/blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/<int:pk>/', views.single_post_page),
+    path('<int:pk>/', views.single_post_page),
     path('', views.index),
 
 ]
